052c9e8a57f88846d93b1777a61bc1b05db4075a
Christoph Buchli <5338859+cbuchli@users.noreply.github.com>
2019-06-14 11:20:41 +0200

    Bugfix: Avoid nullPointerException in authorizedJobController..

--- src/Classes/Controller/Traits/AuthorizedJobController.php
+++ src/Classes/Controller/Traits/AuthorizedJobController.php
@@ -52,7 +52,9 @@ trait AuthorizedJobController
     public function validateJob(): bool
     {
         // server has to be owned by current user or authenticated by jwt token
-        return ($this->user->getId() === $this->job->getOwner()->getId())
-            || (!$this->user && JwtUtility::verifyJobIdentificationToken($this->job, filter_var($this->params['token'], FILTER_SANITIZE_STRING)));
+        return $this->user && $this->job && (
+                ($this->user->getId() === $this->job->getOwner()->getId())
+                || (!$this->user && JwtUtility::verifyJobIdentificationToken($this->job, filter_var($this->params['token'], FILTER_SANITIZE_STRING)))
+            );
     }
 }
