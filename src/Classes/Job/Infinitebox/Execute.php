babbb3629bf9b7f81c0d285161187b1495bba997
Michael Weibel <307427+mweibel@users.noreply.github.com>
2019-08-28 15:20:48 +0200

    rewrite args hiera check to use infinitebox (#62)

--- src/Classes/Job/Infinitebox/Execute.php
+++ src/Classes/Job/Infinitebox/Execute.php
@@ -19,7 +19,7 @@ class Execute extends AbstractExecute
         return (new DispatchConfig())
             ->setFixedReplicaCount(1)// enforce call of dispatch command on every new execution
             ->setImage('hub.helio.dev:4567/helio/runner/infinitebox:latest')
-            ->setArgs(['/bin/sh', '-c', escapeshellcmd('i=0; while true; do echo "$i: $(date)"; i=$((i+1)); sleep 10; done')])
+            ->setArgs(['/bin/sh', '-c', 'i=0; while true; do echo "$i: $(date)"; i=$((i+1)); sleep 10; done'])
             ->setEnvVariables([
                 'HELIO_JOBID' => $this->job->getId(),
                 'HELIO_TOKEN' => JwtUtility::generateToken(null, null, null, $this->job)['token'],
--- tests/Integration/AutoscalerTest.php
+++ tests/Integration/AutoscalerTest.php
@@ -172,8 +172,7 @@ class AutoscalerTest extends TestCase
      */
     public function testArgInHiera(): void
     {
-        $this->markTestSkipped('Args removed from busybox Dockerfile. Do we still need args? this test checks if args is there. 2019-08-27');
-        $this->job->setType(JobType::BUSYBOX);
+        $this->job->setType(JobType::INFINITEBOX);
         $execution = (new Execution())->setJob($this->job)->setStatus(ExecutionStatus::READY);
         $this->infrastructure->getEntityManager()->persist($this->job);
         $this->infrastructure->getEntityManager()->persist($execution);
